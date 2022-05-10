45ca64f42a9e7b9d04b76df950efd13e0e54c1c5
MohamedSolimann <mohamadico@gmail.com>
2022-05-10 16:43:20 +0200

    created endpoint to get all users

--- routes/user-router.js
+++ routes/user-router.js
@@ -25,5 +25,41 @@ router.post("/", async (req, res) => {
   });
   res.status(201).json({ message: "Success", data: newUser });
 });
+router.get("/", async (req, res) => {
+  const users = await getUsers();
+  res.status(200).json({ message: "Success", data: users });
+});
+router.post("/", async (req, res) => {
+  const { username, password, image, bio, age, mobile, videos, email, games } =
+    req.body;
+  const newUser = await createUser({
+    username,
+    password,
+    image,
+    bio,
+    age,
+    mobile,
+    videos,
+    email,
+    games,
+  });
+  res.status(201).json({ message: "Success", data: newUser });
+});
+router.post("/", async (req, res) => {
+  const { username, password, image, bio, age, mobile, videos, email, games } =
+    req.body;
+  const newUser = await createUser({
+    username,
+    password,
+    image,
+    bio,
+    age,
+    mobile,
+    videos,
+    email,
+    games,
+  });
+  res.status(201).json({ message: "Success", data: newUser });
+});
 
 export { router };
